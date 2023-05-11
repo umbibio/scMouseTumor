6f1f6da6dd33f01df09c6694d2f9cd05fe4192b8
Kourosh Zarringhalam <kouroshz@users.noreply.github.com>
2023-05-11 12:01:07 -0400

    updated 3d plot

--- plot_3d.R
+++ plot_3d.R
@@ -16,8 +16,11 @@ getPcaUmapMetaData <- function(S.O){
   umap <- data.frame(umap) %>% dplyr::mutate(Sample = rownames(umap)) %>% 
     transmute(Sample = Sample, UMAP_1 = UMAP_1, UMAP_2 = UMAP_2, UMAP_3 = UMAP_3)
   
-  meta.data <- data.frame(Sample = rownames(S.O@meta.data), customclassif = S.O@meta.data$customclassif, 
-                         clusters = S.O@meta.data$seurat_clusters)
+  # meta.data <- data.frame(Sample = rownames(S.O@meta.data), customclassif = S.O@meta.data$customclassif, 
+  #                        clusters = S.O@meta.data$seurat_clusters)
+  meta.data <- data.frame(Sample = rownames(S.O@meta.data), celltype = S.O@meta.data$cell.types, 
+                          clusters = S.O@meta.data$seurat_clusters)
+  
   meta.data <- left_join(meta.data,
                          pc, by = 'Sample')
   meta.data <- left_join(meta.data, umap, by = 'Sample')
@@ -25,23 +28,25 @@ getPcaUmapMetaData <- function(S.O){
 }
 
 
+S.O <- readRDS('../../data/input/fromJosh/s2_fvb50subset_new.rds')
+S.O@meta.data$cell.types <- S.O@active.ident
 
-pca.tg <- getPcaUmapMetaData(S.O.T4)
+pca.tg <- getPcaUmapMetaData(S.O)
 
 
 
 
-fig <- plot_ly(pca.tg, x = ~UMAP_1, y = ~UMAP_2, z = ~UMAP_3, color = ~customclassif)
+fig <- plot_ly(pca.tg, x = ~UMAP_1, y = ~UMAP_2, z = ~UMAP_3, color = ~celltype)
                #colors = c('#f8766d', '#a2a400', '#00bf7d', '#04b0f6', '#e76bf3')) 
 fig <- fig %>% add_markers(size = 2)
-fig <- fig %>% layout(scene = list(xaxis = list(title = 'PC_1'),
-                                   yaxis = list(title = 'PC_2'),
-                                   zaxis = list(title = 'PC_3')))
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'UMAP_1'),
+                                   yaxis = list(title = 'UMAP_2'),
+                                   zaxis = list(title = 'UMAP_3')))
 
 fig
 
-Idents(S.O.T2) <- 'customclassif'
-FeaturePlot(S.O.T2, features = c("Pdgfra", 'Pdgfrb'), reduction = 'umap', label = T)
+Idents(S.O) <- 'celltype'
+FeaturePlot(S.O, features = c("Pdgfra", 'Pdgfrb'), reduction = 'umap', label = T)
 
 
 PCAs <- pca.tg %>% select(contains('PC'))
